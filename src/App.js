bf1f6254c58d8b0698c342548c36256698bbcd00
murawakimitsuhiro <mitsuhiromurawaki@gmail.com>
2018-07-26 19:20:21 +0900

    Bindの基礎を諸々やってみた

--- src/App.js
+++ src/App.js
@@ -3,19 +3,41 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: 'stateの名前',
+      greetings: ['Hello World']
+    };
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e) {
+    if (e.keyCode === 13) {
+      this.setState({name: e.target.value})
+
+      this.setState({greetings: [...this.state.greetings, 'hello ' + e.target.value]})
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to {this.props.greet} + {this.state.name}</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.state.greetings.map((g,i) => {
+          return <p key={i}>{g}</p>
+        })}
+        <input onKeyDown={this.handleClick}/>
       </div>
     );
   }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App greet={'index.JSから送った値'} />,
+    document.getElementById('root')
+);
+
 registerServiceWorker();
